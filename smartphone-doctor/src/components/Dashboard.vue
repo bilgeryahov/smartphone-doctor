4e527afc2fe1a3692c164fdbc4f795d8d6b575fe
Bilger Yahov <bayahov1@gmail.com>
2018-11-21 22:06:54 +0200

    Implement Dashboard component.

---
+++ smartphone-doctor/src/components/Dashboard.vue
@@ -0,0 +1,25 @@
+<template>
+	<div>
+		<h3>Dashboard</h3>
+		<button
+			name="button"
+			@click="signOut"
+		>
+			Sign Out
+		</button>
+	</div>
+</template>
+
+<script>
+	import { firebaseApp } from '../firebaseApp';
+
+	export default {
+		methods: {
+			signOut() {
+				firebaseApp
+				.auth()
+				.signOut();
+			}
+		}
+	}
+</script>
