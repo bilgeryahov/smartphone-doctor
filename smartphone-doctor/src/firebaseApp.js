9f837b25c7134fc0d108600d88fa91048fd7ab38
bilger-progress <bilger.yahov@progress.com>
2018-11-22 14:44:15 +0200

    Formatting.

--- smartphone-doctor/src/firebaseApp.js
+++ smartphone-doctor/src/firebaseApp.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase';
 
 const config = {
-    apiKey: "AIzaSyALZQeNgV4XO0DA5MWMUas7RZaTjXU1bg4",
-    authDomain: "smartphone-doctor.firebaseapp.com",
-    databaseURL: "https://smartphone-doctor.firebaseio.com",
-    projectId: "smartphone-doctor",
-    storageBucket: "smartphone-doctor.appspot.com",
-    messagingSenderId: "315225363533"
+	apiKey: "AIzaSyALZQeNgV4XO0DA5MWMUas7RZaTjXU1bg4",
+	authDomain: "smartphone-doctor.firebaseapp.com",
+	databaseURL: "https://smartphone-doctor.firebaseio.com",
+	projectId: "smartphone-doctor",
+	storageBucket: "smartphone-doctor.appspot.com",
+	messagingSenderId: "315225363533"
 };
 
 export const firebaseApp = firebase.initializeApp(config);
