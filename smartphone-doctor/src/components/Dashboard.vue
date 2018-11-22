fde04635679c4a935208f7c12705daf25b0aab6b
bilger-progress <bilger.yahov@progress.com>
2018-11-22 15:20:15 +0200

    Formatting.

--- smartphone-doctor/src/components/Dashboard.vue
+++ smartphone-doctor/src/components/Dashboard.vue
@@ -20,14 +20,14 @@ export default {
         .auth()
         .signOut()
         .then(function() {
-			// eslint-disable-next-line
-			console.log("#Dashboard.vue: User successfully signed out.");
+          // eslint-disable-next-line
+          console.log("#Dashboard.vue: User successfully signed out.");
         })
         .catch(function(error) {
-			// eslint-disable-next-line
-			console.error("#Dashboard.vue: Error while signing out the user.");
-			// eslint-disable-next-line
-			console.error(JSON.stringify(error));
+          // eslint-disable-next-line
+          console.error("#Dashboard.vue: Error while signing out the user.");
+          // eslint-disable-next-line
+          console.error(JSON.stringify(error));
         });
     }
   }
